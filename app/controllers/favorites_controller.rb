7ce1d3dc3971052824514d22e7254a164132875e
tsukasatakahashi0614 <tkspp2sko@gmail.com>
2021-07-21 11:31:46 +0000

    非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,15 @@
 class FavoritesController < ApplicationController
+before_action :authenticate_user!
 
  def create
-   post = Post.find(params[:post_id])
-   favorite = current_user.favorites.new(post_id: post.id)
+   @post = Post.find(params[:post_id])
+   favorite = @post.favorites.new(user_id: current_user.id)
    favorite.save
-   redirect_to post_path(post)
  end
- 
+
  def destroy
-   post = Post.find(params[:post_id])
-   favorite = current_user.favorites.find_by(post_id: post.id)
+   @post = Post.find(params[:post_id])
+   favorite = @post.favorites.find_by(user_id: current_user.id)
    favorite.destroy
-   redirect_to post_path(post)
  end
-  
 end
