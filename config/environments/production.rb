9c313b68a1203c31f61a867536dfc5057827ed67
tsukasatakahashi0614 <tkspp2sko@gmail.com>
2021-07-30 06:42:40 +0000

    devise mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,20 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  #mailer setting
+  config.action_mailer.perform_caching = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: "http://35.75.199.157/" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => ENV["Gmail"],
+    :password => ENV["GmailPW"],
+    :authentication => 'login'
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
